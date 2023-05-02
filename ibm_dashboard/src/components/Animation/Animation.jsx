6c015b1d0a29a06223f71244111be7762f8f5068
fernandocamacho <salvadorfernando200@outlook.com>
2023-05-02 17:50:54 -0600

    Working on login

---
+++ ibm_dashboard/src/components/Animation/Animation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Animation.css';
+
+const Animation = () => {
+    return (
+        <div className="animation-container">
+            <div className="animation">
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+            </div>
+        </div>
+  );
+};
+
+export default Animation;
