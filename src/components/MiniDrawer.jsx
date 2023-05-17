58994b278a161478448110b200f31562bae04b5d
fernandocamacho <salvadorfernando200@outlook.com>
2023-05-17 12:50:04 -0600

    basic dashboard

--- src/components/MiniDrawer.jsx
+++ src/components/MiniDrawer.jsx
@@ -116,7 +116,7 @@ const MiniDrawer = () => {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            Mini variant drawer
+            OTTERBOARD
           </Typography>
         </Toolbar>
       </AppBar>
@@ -194,17 +194,8 @@ const MiniDrawer = () => {
           sapien faucibus et molestie ac.
         </Typography>
         <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
+          The widgets that will display the graphs, tables and other information are being worked on.
+          Buttons not final.
         </Typography>
       </Box>
     </Box>
