0ef64baa785897055f76d1dfc8e3f8d98435803a
Rodolfo Bojorquez <rodobp97@gmail.com>
2023-05-23 15:44:55 -0600

    Change DropzoneComponent

--- src/components/DropzoneButton/DropzoneComponent.jsx
+++ src/components/DropzoneButton/DropzoneComponent.jsx
@@ -1,66 +1,61 @@
 import React, { useState } from 'react';
 import Dropzone from 'react-dropzone';
-import './Dropzone.css'
+import axios from 'axios';
+import './Dropzone.css';
 
-const DropzoneComponent = () => {
+
+const DropzoneComponent = ({ onHandle }) => {
     const [file, setFile] = useState(null);
-    const [processedData, setProcessedData] = useState(null);
-
-    function handleDrop(acceptedFiles, rejectedFiles) {
-        console.log('Archivos aceptados:', acceptedFiles);
-        console.log('Archivos rechazados:', rejectedFiles);
-
-        if (rejectedFiles.length > 0) {
-            window.alert('Only CSV files are allowed.');
-            <div className='dropzone.reject'></div>
+  
+    async function handleDrop(acceptedFiles) {
+      const droppedFile = acceptedFiles[0];
+  
+      try {
+        const formData = new FormData();
+        formData.append('file', droppedFile);
+  
+        // Send the file using Axios
+        const response = await axios.post('http://localhost:5000/bucket/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+  
+        // Handle the response from the backend
+        if (response.status === 200) {
+          // File was successfully uploaded
+          console.log('File uploaded successfully');
         } else {
-            setFile(acceptedFiles[0]);
-
-            const formData = new FormData();
-            formData.append('archivo_csv', acceptedFiles[0]);
-            fetch('/api/procesar-csv', {
-                method: 'POST',
-                body: formData,
-            })
-                .then((response) => response.json())
-                .then((data) => {
-                    setProcessedData(data);
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-
-            <div className='dropzone.accept'></div>
+          // File upload failed
+          console.log('File upload failed');
         }
+      } catch (error) {
+        console.log('Error occurred during file upload', error);
+      }
+  
+      setFile(droppedFile);
+      onHandle(droppedFile);
     }
-
+  
     return (
-        <Dropzone 
-            className="cta-button"
-            onDrop={handleDrop}
-            accept={{
-                '.xlsx, .xls': 'application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                '.csv': 'text/csv'}}
-            onDropRejected={handleDrop}
-        >
-            {({ getRootProps, getInputProps }) => (
-                <div {...getRootProps()}>
-                    <input {...getInputProps()} />
-                    {file ? (
-                        <p>File name: {file.name}</p>
-                    ) : (
-                        <div className="cta-button">Drag and drop an CSV file here, or click to select an CSV file</div>
-                    )}
-                    {processedData && (
-                        <div>
-                            <p>Processed data:</p>
-                            <pre>{JSON.stringify(processedData, null, 2)}</pre>
-                        </div>
-                    )}
-                </div>
-            )}
-        </Dropzone>
+      <Dropzone 
+          className="cta-button"
+          onDrop={handleDrop}
+          accept=".csv"
+          onDropRejected={handleDrop}
+      >
+          {({ getRootProps, getInputProps }) => (
+              <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  {file ? (
+                      <p>File name: {file.name}</p>
+                  ) : (
+                      <div className="cta-button">Drag and drop a CSV file here, or click to select a CSV file</div>
+                  )}
+              </div>
+          )}
+      </Dropzone>
     );
-}
-
-export default DropzoneComponent;
+  };
+  
+  export default DropzoneComponent;
