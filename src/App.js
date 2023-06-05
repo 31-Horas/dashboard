63ae67964afd2cf3cd08ce6efdb1c1a60184f1d8
Rodolfo Bojorquez <rodobp97@gmail.com>
2023-06-05 14:37:51 -0600

    Rodo fix.

--- src/App.js
+++ src/App.js
@@ -9,34 +9,33 @@ import "./App.css"
 import axios from 'axios';
 import Topbar from './components/Topbar/Topbar'
 
-function isLoggedIn(){
-  axios.get('http://localhost:5000/auth/signin')
-    .then(response => {
-      // Handle the response from the backend
-      if (response.status === 201) {
-        // User is already logged in
-        console.log('Already logged in');
-        return true;
-      } else if (response.status === 202) {
-        // User is not logged in, redirect to the login page
-        // navigate("/");
-        console.log('Not logged in, redirecting to login');
-        return false;
-      }
-    })
-    .catch(error => {
-      // Handle any errors that occur during the request
-      console.error('Error checking login status:', error);
-    });
+async function isLoggedIn() {
+  try {
+    const response = await axios.get('http://localhost:5000/auth/signin', { withCredentials: true });
+
+    if (response.status === 200) {
+      // User is already logged in
+      console.log('Already logged in');
+      return true;
+    } else if (response.status === 202) {
+      // User is not logged in, redirect to the login page
+      console.log('Not logged in, redirecting to login');
+      return false;
+    }
+  } catch (error) {
+    // Handle any errors that occur during the request
+    console.error('Error checking login status:', error);
+  }
 }
+
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={isLoggedIn ? <Navigate to="/welcome"/> : <LoginForm />} />
+        <Route path="/dashboard" element={isLoggedIn ? <Dashboard/>: <Navigate to="/"/>} />
         <Route path="/welcome" element={isLoggedIn ? <Welcome/>: <Navigate to="/"/>}/>
         <Route path="/signup" element={<SignupForm />}/>
         <Route path="/json" component={JsonPage} />
--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,34 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './Welcome.css' 
 import DropzoneComponent from '../../components/DropzoneButton/DropzoneComponent';
-import { useNavigate } from "react-router-dom";
-import axios from 'axios';
 import Topbar from '../../components/Topbar/Topbar'
 
 const Welcome = () => {
-    const navigate = useNavigate();
-    // useEffect(() => {
-    //     // Send Axios GET request to verify if the user is logged in
-    //     axios.get('http://localhost:5000/auth/signin', {
-    //       withCredentials: true // Enable sending and receiving cookies
-    //     })
-    //       .then(response => {
-    //         // Handle the response from the backend
-    //         if (response.status === 200) {
-    //           // User is already logged in
-    //           console.log('Already logged in');
-    //         } else if (response.status === 401) {
-    //           // User is not logged in, redirect to the login page
-    //           navigate("/");
-    //           console.log('Not logged in, redirecting to login');
-    //         }
-    //       })
-    //       .catch(error => {
-    //         // Handle any errors that occur during the request
-    //         console.error('Error checking login status:', error);
-    //       });
-    //   }, []);
-      
     function handleDrop(file) {
         console.log('File selected:', file);
     }
