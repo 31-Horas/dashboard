6d271043a02404a21c56b6fdd07583649adf55f0
Rodolfo Bojorquez <rodobp97@gmail.com>
2023-06-05 21:21:19 -0600

    Fix :3

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -37,7 +37,7 @@ const LoginForm = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
       
-        const response = await axios.post('http://otterboard.me/auth/signin', {
+        const response = await axios.post('http://otterboard.me:5000/auth/signin', {
             email: email,
             password: password,
             remember: rememberMe
